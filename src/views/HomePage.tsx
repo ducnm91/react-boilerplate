d3573a8a5fe02ffd9a289370a77f0e79f6e92542
Gao <duc.nguyen@nextgenblockchain.net>
2023-01-18 16:33:59 +0400

    revert render prop

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
+import MouseTracker from "components/RenderProp";
 
 function HomePage() {
   const { t } = useTranslation(["translation"]);
@@ -12,6 +13,7 @@ function HomePage() {
         </Helmet>
       </HelmetProvider>
       <h2>{t("Welcome to React")}</h2>
+      <MouseTracker />
     </>
   );
 }
